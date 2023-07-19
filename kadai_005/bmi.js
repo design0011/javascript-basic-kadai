75728a69ece4c6f03b11d3f30a8e139d226a2a8c
design0011 <138834827+design0011@users.noreply.github.com>
2023-07-19 11:51:14 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重、身長、BMIを宣言
+let youWeight;
+let youheight;
+let youBMI;
+// 値の代入
+youWeight = 68;
+youheight = 1.7;
+youBMI = (youWeight / (youheight * youheight));
+
+// BMIをコンソールへ出力
+console.log(youBMI);
